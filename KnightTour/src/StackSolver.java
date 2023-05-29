3de35fc48fe252d4bd76475e4c127e1f9dbcc47e
Matias Barandiaran <115642529+M4mbo@users.noreply.github.com>
2023-05-29 15:23:34 +0200

    Update StackSolver.java

--- KnightTour/src/StackSolver.java
+++ KnightTour/src/StackSolver.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class StackSolver{
@@ -5,7 +6,9 @@ public class StackSolver{
     int n;
     Board board;
     boolean start = false;
+    ArrayList<ArrayList<Position>> avoid;
     boolean[][] visited;
+    int code; //1 to 8 for 8 possible ways a knight can move assuming the best case
 
     Stack<Position> tour;
 
@@ -13,7 +16,8 @@ public class StackSolver{
         this.n = n;
         this.board = board;
         visited = new boolean[n][n];
-        tour = new Stack();
+        tour = new Stack<>();
+        avoid = new ArrayList<>(n*n);
 
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
@@ -24,7 +28,8 @@ public class StackSolver{
 
     public Stack<Position> solve(){
         if(!start){
-            tour.push(new Position(0,0));
+            tour.push(new Position(0,0,0));
+            avoid.add(new ArrayList<>(8));
             start = true;
             return tour;
         }
@@ -33,8 +38,10 @@ public class StackSolver{
 
             if(availableTiles()){
                 chooseNewTile();
+                return tour;
             }else{
                 backtrack();
+                return tour;
             }
         }
 
@@ -43,42 +50,113 @@ public class StackSolver{
 
     public void chooseNewTile(){
 
+        Position lastPos = tour.peek();
+
+        switch(code){
+            case 1:
+                tour.push(new Position(lastPos.row + 1, lastPos.col - 2, tour.size()));
+                visited[lastPos.row + 1][lastPos.col - 2] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 2:
+                tour.push(new Position(lastPos.row - 1, lastPos.col - 2, tour.size()));
+                visited[lastPos.row - 1][lastPos.col - 2] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 3:
+                tour.push(new Position(lastPos.row + 1, lastPos.col + 2, tour.size()));
+                visited[lastPos.row + 1][lastPos.col + 2] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 4:
+                tour.push(new Position(lastPos.row - 1, lastPos.col + 2, tour.size()));
+                visited[lastPos.row - 1][lastPos.col + 2] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 5:
+                tour.push(new Position(lastPos.row - 2, lastPos.col - 1, tour.size()));
+                visited[lastPos.row - 2][lastPos.col - 1] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 6:
+                tour.push(new Position(lastPos.row - 2, lastPos.col + 1, tour.size()));
+                visited[lastPos.row - 2][lastPos.col + 1] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 7:
+                tour.push(new Position(lastPos.row + 2, lastPos.col - 1, tour.size()));
+                visited[lastPos.row + 2][lastPos.col - 1] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+            case 8:
+                tour.push(new Position(lastPos.row + 2, lastPos.col + 1, tour.size()));
+                visited[lastPos.row + 2][lastPos.col + 1] = true;
+                avoid.add(new ArrayList<>(8));
+                break;
+        }
+ 
+    }
+
+    public boolean avoidTile(int row, int col){
+        
+        for(int i = 0; i < avoid.get(tour.size()-1).size() ; i++){
+            if(avoid.get(tour.size()-1).get(i).row == row && avoid.get(tour.size()-1).get(i).col == col){
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     public boolean availableTiles(){
 
         Position lastPos = tour.peek();
 
-        if(lastPos.row + 1 > n && lastPos.col - 2 < 0){
-            if(!visited[lastPos.row + 1][lastPos.col - 2]){
+        if(lastPos.row + 1 < n && lastPos.col - 2 >= 0){
+            if(!visited[lastPos.row + 1][lastPos.col - 2] && !avoidTile(lastPos.row + 1, lastPos.col - 2)){
+                code = 1;
                 return true;
             }
-        }else if (lastPos.row - 1 < 0 && lastPos.col - 2 < 0){
-            if(!visited[lastPos.row - 1][lastPos.col - 2]){
+        }
+        if (lastPos.row - 1 >= 0 && lastPos.col - 2 >= 0){
+            if(!visited[lastPos.row - 1][lastPos.col - 2] && !avoidTile(lastPos.row - 1, lastPos.col - 2)){
+                code = 2;
                 return true;
             }
-        }else if (lastPos.row + 1 > n && lastPos.col + 2 > n){
-            if(!visited[lastPos.row + 1][lastPos.col + 2]){
+        }
+        if (lastPos.row + 1 < n && lastPos.col + 2 < n){
+            if(!visited[lastPos.row + 1][lastPos.col + 2] && !avoidTile(lastPos.row + 1, lastPos.col + 2)){
+                code = 3;
                 return true;
             }
-        }else if (lastPos.row - 1 < 0 && lastPos.col + 2 > n){
-            if(!visited[lastPos.row - 1][lastPos.col + 2]){
+        }
+        if (lastPos.row - 1 >= 0 && lastPos.col + 2 < n){
+            if(!visited[lastPos.row - 1][lastPos.col + 2] && !avoidTile(lastPos.row - 1, lastPos.col + 2)){
+                code = 4;
                 return true;
             }
-        }else if (lastPos.row - 2 < 0 && lastPos.col - 1 < 0){
-            if(!visited[lastPos.row - 2][lastPos.col - 1]){
+        }
+        if (lastPos.row - 2 >= 0 && lastPos.col - 1 >= 0){
+            if(!visited[lastPos.row - 2][lastPos.col - 1] && !avoidTile(lastPos.row - 2, lastPos.col - 1)){
+                code = 5;
                 return true;
             }
-        }else if (lastPos.row - 2 < 0 && lastPos.col + 1 > n){
-            if(!visited[lastPos.row - 2][lastPos.col + 1]){
+        }
+        if (lastPos.row - 2 >= 0 && lastPos.col + 1 < n){
+            if(!visited[lastPos.row - 2][lastPos.col + 1] && !avoidTile(lastPos.row - 2, lastPos.col + 1)){
+                code = 6;
                 return true;
             }
-        }else if (lastPos.row + 2 > n && lastPos.col - 1 < 0){
-            if(!visited[lastPos.row + 2][lastPos.col - 1]){
+        }
+        if (lastPos.row + 2 < n && lastPos.col - 1 >= 0){
+            if(!visited[lastPos.row + 2][lastPos.col - 1] && !avoidTile(lastPos.row + 2, lastPos.col - 1)){
+                code = 7;
                 return true;
             }
-        }else if (lastPos.row + 2 > n && lastPos.col + 1 > n){
-            if(!visited[lastPos.row + 2][lastPos.col + 1]){
+        }
+        if (lastPos.row + 2 < n && lastPos.col + 1 < n){
+            if(!visited[lastPos.row + 2][lastPos.col + 1] && !avoidTile(lastPos.row + 2, lastPos.col + 1)){
+                code = 8;
                 return true;
             }
         }
@@ -88,7 +166,10 @@ public class StackSolver{
     }
 
     public void backtrack(){
-
+        Position lastPos = tour.pop();
+        avoid.remove(lastPos.order);
+        avoid.get(tour.size()-1).add(lastPos);
+        visited[lastPos.row][lastPos.col] = false;
     }
 
 
